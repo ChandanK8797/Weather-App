efb60190fad6128f3a2ced652c2cc99b949cc02b
Delta Student <chandan@email.com>
2026-02-07 22:35:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <p>Welcome to your Weather App!</p>
+    </div>
+  );
+}
+
+export default App;
